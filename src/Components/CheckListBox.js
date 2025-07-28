ec2eec64d52949afff5922125378cf2873256573
Md Mubin <fontdevmubin@gmail.com>
2025-07-28 16:23:49 +0600

    final one

--- src/Components/CheckListBox.js
+++ src/Components/CheckListBox.js
@@ -1,11 +1,17 @@
+import Link from "next/link";
+
 export default function CheckListBox({ checkLists, ctaText }) {
     return (
         <>
             <section className="sticky top-20 shadow-md p-4 mt-2 border border-[#00000033] rounded-lg">
 
-                <button className="w-full py-4 text-xl text-white font-semibold mb-6 bg-green-500 rounded-xl shadow-[inset_0px_0px_20px_#00000055] hover:shadow-[inset_0px_0px_30px_#00000088] duration-300 cursor-pointer">
-                    {ctaText?.name}
-                </button>
+                <p className="text-2xl font-semibold pb-4">1000{"(default)"}</p>
+
+                <Link href={"#"}>
+                    <button className="w-full py-4 text-xl text-white font-semibold mb-6 bg-green-500 rounded-xl shadow-[inset_0px_0px_20px_#00000055] hover:shadow-[inset_0px_0px_30px_#00000088] duration-300 cursor-pointer">
+                        {ctaText?.name}
+                    </button>
+                </Link>
 
                 {
                     checkLists.map((items) => (
